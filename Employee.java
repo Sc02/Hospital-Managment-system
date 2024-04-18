a6d7aef222d5263c0606005ad861332251bd3264
Suchit <98886650+Sc02@users.noreply.github.com>
2024-04-18 07:50:37 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,150 +1,81 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Employee extends JFrame {
 
-    protected static final String patientID = null;
-    private JPanel contentPane;
-    private static String adminID;
-
-    public Employee(String adminID) throws SQLException {
-        Employee.adminID = adminID;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        String urlDB = "jdbc:mysql://localhost:3306/HospitalManagementSystem";
-        String username = "root";
-        String password = "root@123";
-        Connection connection = DriverManager.getConnection(urlDB, username, password);
-
-        if (connection != null) {
-            System.out.println("Connection established");
-        }
-
+    public Employee(String adminID) {
         setTitle("Employee Page");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 800, 400);
-        contentPane = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                // Draw the background image
-                ImageIcon imageIcon = new ImageIcon("doctor_background.jpg");
-                Image image = imageIcon.getImage();
-                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-            }
-        };
-        contentPane.setLayout(new FlowLayout(FlowLayout.CENTER)); // Use absolute layout
-        setContentPane(contentPane);
 
-        // Create sign out button
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new BorderLayout());
+
+        // Navigation Bar
+        JPanel navBar = new JPanel();
+        navBar.setBackground(Color.LIGHT_GRAY);
+        navBar.setLayout(new BorderLayout());
         JButton signOutButton = new JButton("Sign Out");
-        signOutButton.setBounds(650, 10, 120, 30);
-        contentPane.add(signOutButton);
         signOutButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
+                // Add sign out functionality here
                 dispose(); // Close the current window
-                // Open the login page
-                try {
-                    new LoginFrame().setVisible(true);
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+                // Add code to sign out
             }
         });
+        navBar.add(signOutButton, BorderLayout.EAST);
 
-        JPanel dataPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 10));  
-        JLabel empNamLabel = new JLabel("Emp name: ");
-        empNamLabel.setBounds(0, 0, 100, 50);
-        JTextField empNamTextField = new JTextField("test");
-        dataPanel.add(empNamLabel);
-        dataPanel.add(empNamTextField);
-        contentPane.add(dataPanel);
+        contentPane.add(navBar, BorderLayout.NORTH);
 
+        // Add Employee Label
+        JLabel addEmployeeLabel = new JLabel("Add Employee:");
+        JButton addButton = new JButton("Add");
+        addButton.setPreferredSize(new Dimension(80, 30));
 
-        // Create time slot buttons
-        // String[] timeSlots = {"5PM - 6PM", "6PM - 7PM", "7PM - 8PM"};
-        // int yPos = 100; // Initial y-position for time slot buttons
+        // Text Boxes
+        JPanel textFieldsPanel = new JPanel();
+        textFieldsPanel.setLayout(new FlowLayout());
+        JTextField textField1 = new JTextField("Placeholder 1");
+        JTextField textField2 = new JTextField("Placeholder 2");
+        JTextField textField3 = new JTextField("Placeholder 3");
+        JTextField textField4 = new JTextField("Placeholder 4");
+        JTextField textField5 = new JTextField("Placeholder 5");
+        JTextField textField6 = new JTextField("Placeholder 6");
+        JTextField textField7 = new JTextField("Placeholder 7");
+        textFieldsPanel.add(addEmployeeLabel);
+        textFieldsPanel.add(textField1);
+        textFieldsPanel.add(textField2);
+        textFieldsPanel.add(textField3);
+        textFieldsPanel.add(textField4);
+        textFieldsPanel.add(textField5);
+        textFieldsPanel.add(textField6);
+        textFieldsPanel.add(textField7);
+        textFieldsPanel.add(addButton);
 
-        // //get name and designation
-        // try (Statement stmt = connection.createStatement()){
-        //     String nameQuery = "select * from doctor where employeeID = '" + adminID + "'";
-        //     ResultSet namResultSet = stmt.executeQuery(nameQuery);
-        //     String docName = "", spec = "";
-        //     if (namResultSet.next()){
-        //         docName = namResultSet.getString("docName");
-        //         spec = namResultSet.getString("specialisation");
-        //         System.out.println("docName is " + docName);
-        //     }
-        //     JLabel doctorLabel = new JLabel("Welcome " + docName + " of department " + spec);
-        //     doctorLabel.setBounds(50, 50, 300, 50);
-        //     contentPane.add(doctorLabel);
-        // } catch (Exception e) {
-        //     System.out.println(e);
-        // }
+        contentPane.add(textFieldsPanel, FlowLayout.CENTER);
 
-        // for (int i = 0; i < 3; i++) {
-        //     String timeSlot = timeSlots[i];
-        //     JButton timeSlotButton = new JButton(timeSlot);
-        //     timeSlotButton.setBounds(50, yPos, 150, 30);
-        //     contentPane.add(timeSlotButton);
-        //     yPos += 40; // Increment y-position for the next button
+        JPanel deleteDoctorPanel = new JPanel();
+        deleteDoctorPanel.setLayout(new FlowLayout());
+        JLabel deleteEmployeeLabel = new JLabel("Delete Employee:");
+        JButton deleteButton = new JButton("Delete");
+        addButton.setPreferredSize(new Dimension(80, 30));
+        JTextField textField8 = new JTextField("Placeholder 8");
+        deleteDoctorPanel.add(deleteEmployeeLabel);
+        deleteDoctorPanel.add(textField8);
+        deleteDoctorPanel.add(deleteButton);
+        contentPane.add(deleteDoctorPanel, FlowLayout.CENTER);
+        // Add Button
+        // Set button size
 
-        //     //get the slots
-        //     try (Statement stmt = connection.createStatement();) {
-        //         String getWards = "select * from appointment where doctorID = '" + adminID + "' and slot = " + (i + 1);
-        //         ResultSet WardResultSet = stmt.executeQuery(getWards);
-
-        //         int desiredWard = -1;
-        //         if (WardResultSet.next()){
-        //             desiredWard = Integer.parseInt(WardResultSet.getString("wardID"));
-        //             System.out.println("des ward " + desiredWard);
-        //             JLabel wardOption = new JLabel(String.valueOf(desiredWard));
-        //             wardOption.setBounds(250, yPos - 40, 150, 30);
-        //             contentPane.add(wardOption);
-        //         }
-        //         // System.out.println("des ward " + desiredWard);
-
-        //     } catch (Exception e) {
-        //         System.out.println(e);
-        //     }
-        // }
-
-        // // Create "Available Wards" button
-        // JButton availableWardsButton = new JButton("Available Wards");
-        // availableWardsButton.setBounds(50, 300, 150, 30);
-        // contentPane.add(availableWardsButton);
-        // availableWardsButton.addActionListener(new ActionListener() {
-        //     public void actionPerformed(ActionEvent e) {
-        //         dispose(); // Close the current window
-        //         try {
-        //             // Open the WardPage
-        //             new WardPage(patientID).setVisible(true);
-        //         } catch (SQLException ex) {
-        //             ex.printStackTrace();
-        //         }
-            // }
-        // });
+        setContentPane(contentPane);
     }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
-            try {
-                Employee frame = new Employee(adminID);
-                frame.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            new Employee("admin123").setVisible(true);
         });
     }
 }
